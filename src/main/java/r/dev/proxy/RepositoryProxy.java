886159af1ba7092ec1112534f7e18cc75dcf46d0
ReGeorged <giorgi.bazierashvili@gmail.com>
2024-01-20 11:11:51 -0800

    made proxy generic

--- src/main/java/r/dev/proxy/RepositoryProxy.java
+++ src/main/java/r/dev/proxy/RepositoryProxy.java
@@ -3,21 +3,17 @@ package r.dev.proxy;
 import jakarta.persistence.EntityManager;
 import r.dev.handlers.TRepositoryInvocationHandler;
 import r.dev.irepository.ITRepository;
-import r.repositories.UserRepository;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-public class UserRepositoryProxy {
-
+public class RepositoryProxy<T extends ITRepository<E, K>, E, K> {
     private EntityManager entityManager;
 
-    public UserRepositoryProxy(EntityManager entityManager) {
+    public RepositoryProxy(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
-    public <T extends ITRepository<E, K>, E, K> T createProxy(Class<T> repositoryInterface, Class<E> entityClass) {
+    public T createProxy(Class<T> repositoryInterface, Class<E> entityClass) {
         return (T) Proxy.newProxyInstance(
                 repositoryInterface.getClassLoader(),
                 new Class<?>[]{repositoryInterface},
--- src/test/java/proxy/RepositoryProxyTest.java
+++ src/test/java/proxy/RepositoryProxyTest.java
@@ -2,29 +2,23 @@ package proxy;
 
 import org.testng.annotations.Test;
 import r.dev.providers.PersistenceProviderFactory;
-import r.dev.proxy.UserRepositoryProxy;
-import r.dev.repository.TRepository;
+import r.dev.proxy.RepositoryProxy;
 import r.entities.UsersEntity;
 import r.repositories.UserRepository;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
-public class UserRepositoryProxyTest {
+public class RepositoryProxyTest {
 
     @Test
     public void testProxy() {
-
-       var entityManager  = PersistenceProviderFactory.getInstance("postgres").createEntityManager();
-        UserRepositoryProxy proxyFactory = new UserRepositoryProxy(entityManager);
-
+        var entityManager  = PersistenceProviderFactory.getInstance("postgres").createEntityManager();
+        RepositoryProxy<UserRepository, UsersEntity, Long> proxyFactory = new RepositoryProxy<>(entityManager);
         UserRepository userRepository = proxyFactory.createProxy(UserRepository.class, UsersEntity.class);
-
-
-        // Use the proxy to perform operations and verify that it works as expected
         var userInDb = userRepository.findById(1L).orElse(null);
-
         assertNotNull(userInDb);
+        System.out.printf("User in db: %s%n", userInDb.getUsername());
     }
 }
 
