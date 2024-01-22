d203b0e21a0a4b3ead241d7f69e05152d85c1fa0
ReGeorged <giorgi.bazierashvili@gmail.com>
2024-01-22 03:06:48 -0800

    added class graph for finding classes annotated with @Steps

--- src/main/java/r/demo/Application.java
+++ src/main/java/r/demo/Application.java
@@ -1,14 +1,16 @@
 package r.demo;
 
+import r.steps.UserSteps;
+
 public class Application {
     public static void main(String[] args) {
-         try {
-            ApplicationContext applicationContext = new ApplicationContext(Application.class);
-            var userSteps = applicationContext.getInstance(UserSteps.class);
-            System.out.println(userSteps.getUserName());
+        try {
+            ApplicationContext applicationContext = new ApplicationContext();
+            var userServices = applicationContext.getInstance(UserSteps.class);
+            System.out.println(userServices.getFirstUserName());
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    }
+}
