46c60cda1eaf83c90b5664cd32425f01318f5a58
ReGeorged <giorgi.bazierashvili@gmail.com>
2024-01-22 02:19:30 -0800

    updated ApplicationContex

--- src/main/java/r/demo/ApplicationContext.java
+++ src/main/java/r/demo/ApplicationContext.java
@@ -1,27 +1,21 @@
 package r.demo;
 
-
+import j.di2.Injector;
 import org.burningwave.core.assembler.ComponentContainer;
 import org.burningwave.core.classes.ClassHunter;
 import org.burningwave.core.classes.SearchConfig;
 import r.dev.annotations.Repository;
 import r.dev.annotations.Steps;
-import r.dev.irepository.ITRepository;
-import r.dev.providers.PersistenceProviderFactory;
-import r.dev.proxy.RepositoryProxy;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class ApplicationContext {
-
     private final Map<Class<?>, Object> objectRegistryMap = new HashMap<>();
+    private final RepositoryInjector repositoryInjector = new RepositoryInjector();
 
     public ApplicationContext(Class<?> clazz) throws ClassNotFoundException, IOException {
         Class<?>[] classes = getClasses(clazz.getPackageName(), true);
@@ -37,6 +31,23 @@ public class ApplicationContext {
         }
     }
 
+    public Class<?>[] getClasses(String packageName, boolean recursive) throws ClassNotFoundException, IOException {
+        ComponentContainer componentConatiner = ComponentContainer.getInstance();
+        ClassHunter classHunter = componentConatiner.getClassHunter();
+        String packageRelPath = packageName.replace(".", "/");
+        SearchConfig config = SearchConfig.forResources(
+                packageRelPath
+        );
+        if (!recursive) {
+            config.findInChildren();
+        }
+
+        try (ClassHunter.SearchResult result = classHunter.findBy(config)) {
+            Collection<Class<?>> classes = result.getClasses();
+            return classes.toArray(new Class[classes.size()]);
+        }
+    }
+
     public <T> T getInstance(Class<T> clazz) throws Exception {
         T object = (T) objectRegistryMap.get(clazz);
 
@@ -49,48 +60,12 @@ public class ApplicationContext {
     private <T> void injectAnnotatedFields(T object, Field[] declaredFields) throws Exception {
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(Repository.class)) {
+                Object repositoryInstance = repositoryInjector.createRepositoryInstance(field);
+                boolean accessible = field.isAccessible();
                 field.setAccessible(true);
-
-                Object repository = createRepositoryInstance(field);
-                objectRegistryMap.put(field.getType(), repository);
-
-                field.set(object, repository);
-                injectAnnotatedFields(repository, field.getType().getDeclaredFields());
+                field.set(object, repositoryInstance);
+                field.setAccessible(accessible);
             }
-        }
-    }
-
-    private Object createRepositoryInstance(Field field) throws Exception {
-        String databaseName = field.getAnnotation(Repository.class).value();
-        var entityManager = PersistenceProviderFactory.getInstance(databaseName).createEntityManager();
-        RepositoryProxy proxyFactory = RepositoryProxy.getInstance(entityManager);
-        // Get the actual entity class from the field's generic type
-        Class<?> fieldType = field.getType();
-        Class<?> entityClass = null;
-
-        for (Type genericInterface : fieldType.getGenericInterfaces()) {
-            if (genericInterface instanceof ParameterizedType) {
-                ParameterizedType paramType = (ParameterizedType) genericInterface;
-                if (paramType.getRawType() == ITRepository.class) {
-                    entityClass = (Class<?>) paramType.getActualTypeArguments()[0];
-                }
-            }
-        }
-        return proxyFactory.createProxy(fieldType, entityClass);
-    }
-
-    public Class<?>[] getClasses(String packageName, boolean recursive) throws ClassNotFoundException, IOException {
-        ComponentContainer componentContainer = ComponentContainer.getInstance();
-        ClassHunter classHunter = componentContainer.getClassHunter();
-        String packageRelPath = packageName.replace(".", "/");
-        SearchConfig config = SearchConfig.forResources(packageRelPath);
-        if (!recursive) {
-            config.findInChildren();
-        }
-
-        try (ClassHunter.SearchResult result = classHunter.findBy(config)) {
-            Collection<Class<?>> classes = result.getClasses();
-            return classes.toArray(new Class[classes.size()]);
         }
     }
 }
